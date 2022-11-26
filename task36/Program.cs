cd49a57ae10a20f426a438bd96abcf970402f879
Sergio <sergio977@rambler.ru>
2022-11-26 21:42:09 +0400

    task36

---
+++ task36/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
+*/
+int[] GenerateRandomArray(int length, int startPoint, int endPoint)
+{
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint + 1);
+    }
+    return resultArray;
+}
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int findSumOddElements(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+int[] array = GenerateRandomArray(6, -10, 10);
+printArray(array);
+int count = findSumOddElements(array);
+Console.WriteLine($"Сумма элементов стоящих на нечетных позициях равна {count}");
+
