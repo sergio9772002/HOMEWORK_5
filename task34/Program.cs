bf6a8eb7563d9fbf0807f475cf55b7642427d66e
Sergio <sergio977@rambler.ru>
2022-11-26 21:29:05 +0400

    task34 new

--- task34/Program.cs
+++ task34/Program.cs
@@ -3,3 +3,50 @@
 
 [345, 897, 568, 234] -> 2
 */
+
+int[] GenerateRandomArray(int length, int startPoint = 100, int endPoint = 999)
+{
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint + 1);
+    }
+    return resultArray;
+}
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int findEvenElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int[] array = GenerateRandomArray(10);
+printArray(array);
+int count = findEvenElements(array);
+if (count > 0)
+{
+Console.WriteLine($"Количество четных чисел равно {count}");
+}
+else
+{
+    Console.WriteLine($"Четных чисел нет");
+}
